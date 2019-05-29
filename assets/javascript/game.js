db739f5003073079b14ec069900373e1d39efe10
matkuh <matkuh@gmail.com>
2019-05-28 21:13:20 -0700

    create reset function(reset button random values and target number, set yourscore to 0, create checkscore function(check to see if win condition  met, wins++ if so, loss++ if not, reset function)

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,5 @@
 var targetNumber = "";
-var yourScore = "";
+var yourScore = 0;
 var wins = 0;
 var losses = 0;
 
@@ -16,9 +16,31 @@ var img2Val = getRndInteget(1, 12)
 var img3Val = getRndInteget(1, 12)
 var img3Val = getRndInteget(1, 12)
 
+function reset() {
+    var targetNumber = getRndInteger(19, 120)
+    var img1Val = getRndInteger(1, 12)
+    var img2Val = getRndInteget(1, 12)
+    var img3Val = getRndInteget(1, 12)
+    var img3Val = getRndInteget(1, 12)
+    var yourScore = 0;
+}
 
+function checkScore() {
+    if ((yourScore) === (targetNumber));
+    alert("Nice Job! You've successfully made the potion")
+    wins++
+    reset();
+    
+    if ((yourScore) > (targetNumber));
+    alert("The witch's cauldron explodes in your face, Boom!!! You've added to much!!!")
+    losses++
+    reset();
+}
 
-
+$("#img1").on("click", function() {
+    yourScore + img1Val; 
+    checkScore();
+    
 
 
 });
