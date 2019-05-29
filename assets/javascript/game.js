33de8debe17d4de62aa1b1d967a4e2971917b87d
matkuh <matkuh@gmail.com>
2019-05-28 22:39:28 -0700

    add .html for score, targetnumber, wins, and losses under check score function to update values on corresponding html, also added = after yourScore + which was throwing me off for awhile

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -11,46 +11,53 @@ function getRndInteger(min, max) {
 
 var targetNumber = getRndInteger(19, 120)
 
-var img1Val = getRndInteger(1, 12)
-var img2Val = getRndInteget(1, 12)
-var img3Val = getRndInteget(1, 12)
-var img3Val = getRndInteget(1, 12)
+var img1Val = getRndInteger(1, 13)
+var img2Val = getRndInteger(1, 13)
+var img3Val = getRndInteger(1, 13)
+var img4Val = getRndInteger(1, 13)
 
 function reset() {
     var targetNumber = getRndInteger(19, 120)
-    var img1Val = getRndInteger(1, 12)
-    var img2Val = getRndInteget(1, 12)
-    var img3Val = getRndInteget(1, 12)
-    var img3Val = getRndInteget(1, 12)
+    var img1Val = getRndInteger(1, 13)
+    var img2Val = getRndInteger(1, 13)
+    var img3Val = getRndInteger(1, 13)
+    var img4Val = getRndInteger(1, 13)
     var yourScore = 0;
 }
 
 function checkScore() {
-    if ((yourScore) === (targetNumber));
+    if ((yourScore) === (targetNumber)) {
     alert("Nice Job! You've successfully made the potion")
     wins++
     reset();
     
-    if ((yourScore) > (targetNumber));
+    if ((yourScore) > (targetNumber))
     alert("The witch's cauldron explodes in your face, Boom!!! You've added to much!!!")
     losses++
     reset();
 }
+    $("#currentScore").html(yourScore);
+    $("#targNumb").html(targetNumber);
+    $("#wins").html(wins);
+    $("#losses").html(losses);
+}
+
+checkScore();
 
 $("#img1").on("click", function() {
-    yourScore + img1Val; 
+    yourScore += img1Val;
     checkScore();
 })
 $("#img2").on("click", function() {
-    yourScore + img2Val; 
+    yourScore += img2Val; 
     checkScore();
 })
 $("#img3").on("click", function() {
-    yourScore + img3Val; 
+    yourScore += img3Val; 
     checkScore();
 })
 $("#img4").on("click", function() {
-    yourScore + img4Val; 
+    yourScore += img4Val; 
     checkScore();
 })
 
